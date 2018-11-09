3ad6cf38451142f83da2eebe7b15a3ec7f27e488
Joshua Tucker <thejltucker@gmail.com>
2018-11-09 10:13:44 -0500

    Tests in class

---
+++ 08_Repository_Pattern_Tests/StreamingContentTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using _08_Repository_Pattern;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _08_Repository_Pattern_Tests
+{
+    [TestClass]
+    public class StreamingContentTests
+    {
+        [TestMethod]
+        public void StreamingContent_SetTitle_ShouldBeCorrectString()
+        {
+            //--Arrange
+            StreamingContent content = new StreamingContent();
+            content.Title = "Bolt";
+
+            //--Act
+            var actual = content.Title;
+            var expected = "Bolt";
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void StreamingContent_SetIsMatureBoolean_ShouldReturnTrue()
+        {
+            //--Arrange
+            StreamingContent content = new StreamingContent();
+            content.IsMature = true;
+
+            //--Act
+            var actual = content.IsMature;
+            var expected = true;
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void StreamingContent_Constructor_ShouldHoldCorrectValues()
+        {
+            //--Arrange
+            StreamingContent content = new StreamingContent("Star Wars", "Space Adventure", 5, false);
+
+            //--Act
+            var actual = content.Title;
+            var expected = "Star Wars";
+
+            var actualTwo = content.IsMature;
+            var expectedTwo = false;
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expectedTwo, actualTwo);
+
+        }
+    }
+}
